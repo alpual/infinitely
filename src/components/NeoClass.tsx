36ccf7d8886b1b86a25fc17ec1b19a35a85c6687
PWright <pwright@fencycoin.com>
2018-05-30 17:47:06 -0600

    pulling in json

--- src/components/NeoClass.tsx
+++ src/components/NeoClass.tsx
@@ -5,27 +5,41 @@ export interface IProps {
   API_Key: string;
 }
 
+export interface INeoStatus {
+	neo_json: string;
+	status: number;
+};
 
-class NeoClass extends React.Component<IProps, object> {
+
+class NeoClass extends React.Component<IProps, INeoStatus > {
 	public API_KEY: string
 
 	constructor (props: any) {
 	  super(props);
 	  this.API_KEY = this.props.API_Key;
+	  this.state= {
+	  	neo_json: "no-json-yet",
+	  	status: 0
+	  }
 	}
 	
 	public render() {
-		const query = "https://api.nasa.gov/neo/rest/v1/feed/today?detailed=true" +
-			"&api_key=UmwJQ1PsgKzUDRfptPJH1T9CbveNXUj4rH39sjEI&api_key=" + this.API_KEY;
-		let NeoQuery = "";
+		const query = "https://api.nasa.gov/neo/rest/v1/feed/today?detailed=true&api_key=" + this.API_KEY;
+
 		// tslint:disable-next-line
-		axios.get(query).then(response => console.log(NeoQuery = response.request.response));
+		axios.get(query).then(
+			response => this.setState( { 
+				neo_json: response.request.response, 
+				status: response.status	
+			})
+		);
 
 		return (
 		  <div className="hello">
 			<div className="greeting">
 			  Hello {this.API_KEY}<br/>
-			  Query retured: {NeoQuery.toString()}
+			  Query Status: {this.state.status}<br/>
+			  Query retured: {this.state.neo_json}
 			</div>
 		  </div>
 		);
