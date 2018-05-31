48cf947d25dfdc8fe54e6e96616aa7ee568568dd
PWright <pwright@fencycoin.com>
2018-05-31 00:07:02 -0600

    need to pass data between different components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as WebFont from 'webfontloader';
+import AsteroidNav from './components/AsteroidNav';
+import AsteroidSizeVis from './components/AsteroidSizeVis';
 import NeoClass from './components/NeoClass';
+
 import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
+WebFont.load({
+  google: {
+    families: ['Anonymous Pro:300,400,700', 'monospace']
+  }
+});
+
 ReactDOM.render(
-  <NeoClass API_Key="UmwJQ1PsgKzUDRfptPJH1T9CbveNXUj4rH39sjEI"/>,
-  document.getElementById('root') as HTMLElement
+	<div className="appWrapper">
+		<h1>Today's Near Earth Objects</h1>
+		<div id="NeoJson">
+	  		<NeoClass API_Key="UmwJQ1PsgKzUDRfptPJH1T9CbveNXUj4rH39sjEI"/> 
+	  	</div>
+	  	<AsteroidSizeVis size={187.85} height={600} width={600}/>
+		<AsteroidNav/>
+  	</div>,
+	document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
