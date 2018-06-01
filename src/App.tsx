c992c38962061f00ce3535b3024ce2b2380a21eb
PWright <pwright@fencycoin.com>
2018-05-31 20:01:29 -0600

    counter works

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
+import AsteroidNav from './components/AsteroidNav';
+import AsteroidSizeVis from './components/AsteroidSizeVis';
+import NeoClass from './components/NeoClass';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="appWrapper">
+        <h1>Today's Near Earth Objects</h1>
+        <div id="NeoJson">
+            <NeoClass API_Key="UmwJQ1PsgKzUDRfptPJH1T9CbveNXUj4rH39sjEI"/> 
+          </div>
+          <AsteroidSizeVis size={187.85} height={600} width={600}/>
+        <AsteroidNav/>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as WebFont from 'webfontloader';
-import AsteroidNav from './components/AsteroidNav';
-import AsteroidSizeVis from './components/AsteroidSizeVis';
-import NeoClass from './components/NeoClass';
-
+import App from './App'
 import './index.css';
-
 import registerServiceWorker from './registerServiceWorker';
 
-WebFont.load({
-  google: {
-    families: ['Anonymous Pro:300,400,700', 'monospace']
-  }
-});
+
 
 ReactDOM.render(
-	<div className="appWrapper">
-		<h1>Today's Near Earth Objects</h1>
-		<div id="NeoJson">
-	  		<NeoClass API_Key="UmwJQ1PsgKzUDRfptPJH1T9CbveNXUj4rH39sjEI"/> 
-	  	</div>
-	  	<AsteroidSizeVis size={187.85} height={600} width={600}/>
-		<AsteroidNav/>
-  	</div>,
+	<App/>,
 	document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
