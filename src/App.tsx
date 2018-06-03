f528bcd00216e51b1b2b05e1de756e3cb8dc484e
PWright <pwright@fencycoin.com>
2018-06-02 21:02:02 -0600

    grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,31 +14,37 @@ class App extends React.Component<{}, IAsteroidStatus> {
   constructor(props:any, context:any) {
     super(props,context)
 
-    this.state = { asteroidNum:0, asteroidSize:0 }
+    this.state = { asteroidNum:0, asteroidSize:100 }
   }
 
 
   public render() {
     return (
       <div className="appWrapper">
-        <h1>Today's Near Earth Objects</h1>
-        <div id="NeoJson">
-            <NeoClass API_Key="UmwJQ1PsgKzUDRfptPJH1T9CbveNXUj4rH39sjEI"/> 
-          </div>
 
+        <h1 className="headsUp">Today's Near Earth Objects</h1>
+
+
+        <div id="sizeVis" className="vis">
+          Asteroid Size Comparison
           <AsteroidSizeVis 
             size={this.state.asteroidSize} 
             height={600} 
             width={600}
             asteroidNum={this.state.asteroidNum}
           />
-          <AsteroidNav 
-            asteroidNum={this.state.asteroidNum}
-            asteroidSize={this.state.asteroidSize}
-            increment={
-              () => this.setState({...this.state, asteroidSize: this.state.asteroidSize+5}) /* tslint:disable-line */
-            }
+        </div>
+        <div className="vis">Upcoming Vis</div>
+        <AsteroidNav 
+          asteroidNum={this.state.asteroidNum}
+          asteroidSize={this.state.asteroidSize}
+          increment={
+            () => this.setState({...this.state, asteroidSize: this.state.asteroidSize+5}) /* tslint:disable-line */
+          }
         />
+        <div id="NeoJson">
+          <NeoClass API_Key="UmwJQ1PsgKzUDRfptPJH1T9CbveNXUj4rH39sjEI"/> 
+        </div>
       </div>
     );
   }
